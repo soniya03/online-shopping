da72390a9af8ad445e131ff60441beaf0dd74a0d
Soniya Vijay <soniyasvijaywargiya@gmail.com>
2017-09-13 13:02:24 +0530

    Successfully loaded 1st landing page

---
+++ onlineshopping/src/main/java/com/soniya/onlineshopping/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.soniya.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		ModelAndView modelAndView=new ModelAndView("page");
+		modelAndView.addObject("greeting","Welcome to spring web mvc");
+		return modelAndView;
+	}
+	
+}
+
